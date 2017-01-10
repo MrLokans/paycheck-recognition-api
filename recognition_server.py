8f2fe1e99adca4b7f7ebfdbcc3ead74a902a570f
MrLokans <trikster1911@gmail.com>
2017-01-10 20:36:37 +0300

    Added bridge between flask web app and recognition service.

--- recognition_server.py
+++ recognition_server.py
@@ -8,6 +8,9 @@ from flask import (
 )
 from werkzeug.utils import secure_filename
 
+from recognition_service import RecognizerService
+
+
 UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
@@ -19,8 +22,11 @@ if not os.path.exists(UPLOAD_FOLDER):
                 .format(UPLOAD_FOLDER))
     os.mkdir(UPLOAD_FOLDER)
 
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['JSON_AS_ASCII'] = False
+recognizer = RecognizerService()
 
 
 def allowed_file(filename):
@@ -38,10 +44,15 @@ def recognize():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             output_fname = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            res = file.save(output_fname)
+            file.save(output_fname)
+            recognized_text = recognizer.recognize_image(output_fname)
+            try:
+                os.remove(output_fname)
+            except:
+                logger.exception("Error removing file ({})"
+                                 .format(output_fname))
             # TODO (mrlokans) add celery/RQ task
-            return jsonify({"status": "file {} successfully uploaded"
-                                      .format(file.filename)})
+            return jsonify({"recognized": recognized_text})
     return '''
     <!doctype html>
     <title>Upload new File</title>
@@ -51,6 +62,7 @@ def recognize():
          <input type=submit value=Upload>
     </form>
     '''
+
 
 if __name__ == '__main__':
     app.run(debug=True)
